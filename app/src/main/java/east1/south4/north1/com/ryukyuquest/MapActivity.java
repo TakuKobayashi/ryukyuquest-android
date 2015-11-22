95fb15539191fb5a4287c0690283507132c554b6
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2015-11-23 06:00:27 +0900

    バトルまではなんとか繊維できた

--- app/src/main/java/east1/south4/north1/com/ryukyuquest/MapActivity.java
+++ app/src/main/java/east1/south4/north1/com/ryukyuquest/MapActivity.java
@@ -1,7 +1,9 @@
 package east1.south4.north1.com.ryukyuquest;
 
+import android.content.SharedPreferences;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -12,9 +14,17 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import java.util.HashMap;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MapActivity extends AppCompatActivity {
     private MediaPlayer mBg;
+    private static final int GotoNextTime = 1000;
+    private Handler mHandler;
+    private WebView webview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,18 +32,44 @@ public class MapActivity extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.map_activity_view);
-        WebView webview = (WebView) findViewById(R.id.mapWebView);
+        mHandler = new Handler();
+        webview = (WebView) findViewById(R.id.mapWebView);
         webview.getSettings().setJavaScriptEnabled(true);
+        webview.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                return false;
+            }
+        });
         webview.loadUrl(Config.ROOT_URL + "map");
         mBg = MediaPlayer.create(this, R.raw.field);
         mBg.setLooping(true);
         mBg.start();
+        Timer timer = new Timer();
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                mHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        mBg.release();
+                        mBg = null;
+                        mBg = MediaPlayer.create(MapActivity.this, R.raw.lastboss);
+                        mBg.setLooping(true);
+                        mBg.start();
+                        SharedPreferences sp = Preferences.getCommonPreferences(MapActivity.this);
+                        webview.loadUrl(Config.ROOT_URL + "battle/encount?mst_monster_id=1&authToken=" + sp.getString("authToken", ""));
+                    }
+                });
+            }
+        };
+        timer.schedule(task, GotoNextTime);
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        ApplicationHelper.releaseWebView((WebView) findViewById(R.id.mapWebView));
+        ApplicationHelper.releaseWebView(webview);
         mBg.release();
     }
 }

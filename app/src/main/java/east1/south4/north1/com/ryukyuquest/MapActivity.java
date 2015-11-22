c406094981f8cf7a34bdbb8d1f61584190fb4de2
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2015-11-22 16:01:37 +0900

    サーバーにあるGoogle Mapのページを見ることができた

--- app/src/main/java/east1/south4/north1/com/ryukyuquest/MapActivity.java
+++ app/src/main/java/east1/south4/north1/com/ryukyuquest/MapActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.webkit.WebView;
 
 public class MapActivity extends AppCompatActivity {
 
@@ -15,38 +16,15 @@ public class MapActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.map_activity_view);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        WebView webview = (WebView) findViewById(R.id.mapWebView);
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.loadUrl(Config.ROOT_URL + "map");
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    protected void onDestroy() {
+        super.onDestroy();
+        ApplicationHelper.releaseWebView((WebView) findViewById(R.id.mapWebView));
     }
 }
